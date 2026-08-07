d77973e5023f2c5a91378652b49009336c3f4c36
salmanhyder2233-arch <salmanhyder2233@gmail.com>
2026-08-07 20:05:34 +0500

    Show join_reason in admin dashboard

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -43,12 +43,13 @@
             </div>
             <p><strong>Email:</strong> {{ $application->email }}</p>
             <p><strong>Applying for:</strong> {{ $application->role }}</p>
-            <p><strong>Join Reason:</strong></p>
-            <details class="text-toggle">
-                <summary>{{ \Illuminate\Support\Str::limit($application->join_reason, 80) }}</summary>
-                <p>{{ $application->join_reason }}</p>
-            </details>            
+            <p><strong>Join Reason:</strong> {{ $application->join_reason }}</p>
             <p>{{ $application->message }}</p>
+            <details class="text-toggle">
+                <summary>{{ \Illuminate\Support\Str::limit($application->message, 80) }}</summary>
+                <p>{{ $application->message }}</p>
+            </details>            
+
 
             <form method="POST" action="{{ url('/admin/application/'.$application->id) }}" onsubmit="return confirm('Delete this application?');">
                 @csrf
