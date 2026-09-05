3a71587c809b0c241ffd022db637f06e97b36edb
salmanhyder2233-arch <salmanhyder2233@gmail.com>
2026-09-05 12:45:50 +0500

    3.5 SiteSettings

---
+++ database/migrations/2026_09_04_153447_create_site_settings_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('site_settings', function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('goal_amount')->default(100000);
+        $table->unsignedBigInteger('raised_amount')->default(0);
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('site_settings');
+    }
+};
